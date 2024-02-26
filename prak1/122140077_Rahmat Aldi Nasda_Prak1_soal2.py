d8ee20bfb1a13441fc72673ca073afd91509ecdc
Rahmat Aldi Nasda <rahmataldin40@gmail.com>
2024-02-26 15:27:19 +0700

    Praktikum PBO 1

--- prak1/122140077_Rahmat Aldi Nasda_Prak1_soal2.py
+++ prak1/122140077_Rahmat Aldi Nasda_Prak1_soal2.py
@@ -7,12 +7,12 @@ phi = 3.14
 def lingkaran (r):
     if r < 0:
         print ("Tidak boleh menginput bilangan negatif atau dibawah nol!")
-
-    keliling = 2 * phi * r
-    luas = phi * r * r
-
-    print ("Keliling dari lingkaran adalah: ", keliling)     
-    print ("Luas dari lingkaran adalah: ", luas)
+    else:
+        keliling = 2 * phi * r
+        luas = phi * r * r
+    
+        print ("Keliling dari lingkaran adalah: ", keliling)     
+        print ("Luas dari lingkaran adalah: ", luas)
 
 r = float(input("Masukkan nilai jari-jari lingkaran: "))
 
